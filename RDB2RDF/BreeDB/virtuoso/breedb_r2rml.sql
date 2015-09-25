1b392c5a48fb40321b822a43682076a51a9c767a
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2015-09-25 13:49:43 +0200

    Updated SQL script including R2RML: BreeDB pp_accession table columns mapped now to the Germplasm and WGS84 Geo Positioning ontologies.

--- RDB2RDF/BreeDB/virtuoso/breedb_r2rml.sql
+++ RDB2RDF/BreeDB/virtuoso/breedb_r2rml.sql
@@ -1,10 +1,20 @@
+--
+-- RDF View on relational data in BreeDB using R2RML mapping language.
+--
+-- Author: Arnold Kuzniar
+--
+
+-- clear graphs
 SPARQL CLEAR GRAPH <http://temp/germplasm>;
 SPARQL CLEAR GRAPH <http://example.com/germplasm/>;
 
+-- insert R2RML into a temporary graph
 DB.DBA.TTLP('
 @prefix rr: <http://www.w3.org/ns/r2rml#> .
 @prefix exa: <http://example.com/ns#> .
 @prefix germplasm: <http://example.com/germplasm#> .
+@prefix geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> .
+@prefix gterm: <http://purl.org/germplasm/germplasmTerm#> .
 
 <exa:#TriplesMap1>
     a rr:TriplesMap;
@@ -19,8 +29,14 @@ DB.DBA.TTLP('
     rr:subjectMap
     [
       rr:template "http://example.com/germplasm/{accessionID}";
-      rr:class exa:germplasm;
+      rr:class gterm:GermplasmAccession;
       rr:graph <http://example.com/germplasm/>;
+    ];
+
+    rr:predicateObjectMap
+    [
+      rr:predicate gterm:germplasmID;
+      rr:objectMap [ rr:column "accessionID" ];
     ];
 
     rr:predicateObjectMap
@@ -28,11 +44,37 @@ DB.DBA.TTLP('
       rr:predicate germplasm:name;
       rr:objectMap [ rr:column "accessionName" ];
     ];
+
+    rr:predicateObjectMap
+    [
+      rr:predicate geo:lat;
+      rr:objectMap [ rr:column "gpsLat"; rr:datatype xsd:decimal ];
+    ];
+
+    rr:predicateObjectMap
+    [
+      rr:predicate geo:long;
+      rr:objectMap [ rr:column "gpsLong"; rr:datatype xsd:decimal ];
+    ];
+
+    rr:predicateObjectMap
+    [
+      rr:predicate geo:alt;
+      rr:objectMap [ rr:column "elevation"; rr:datatype xsd:decimal ];
+    ];
 .
-', 'http://temp/germplasm', 'http://temp/germplasm' )
+', 'http://temp/germplasm', 'http://temp/germplasm')
 ;
 
+-- sanity checks
 SELECT DB.DBA.R2RML_TEST('http://temp/germplasm');
 DB.DBA.OVL_VALIDATE ('http://temp/germplasm', 'http://www.w3.org/ns/r2rml#OVL');
-EXEC('SPARQL ' || DB.DBA.R2RML_MAKE_QM_FROM_G ('http://temp/germplasm'));
-SPARQL SELECT * FROM <http://example.com/germplasm/> WHERE { ?s ?p ?o };
+
+-- convert R2RML into Virtuoso's own Linked Data Views script
+EXEC('SPARQL ' || DB.DBA.R2RML_MAKE_QM_FROM_G('http://temp/germplasm'));
+
+-- graph queries
+
+-- SPARQL SELECT * FROM <http://example.com/germplasm/> WHERE { ?s ?p ?o } LIMIT 10;
+-- SPARQL SELECT * FROM <http://example.com/germplasm/> WHERE { ?s ?p ?o . FILTER(datatype(?o) = xsd:decimal) } LIMIT 10;
+
