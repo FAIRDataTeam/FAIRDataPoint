c6cefa5a5922ae1ebdd9b6e05ec9e4647b0bb797
arnikz <a.kuzniar@esciencecenter.nl>
2015-11-25 19:14:38 +0100

    Added a Python-based minimalistic FAIR Data Point Service including Swagger API documentation.

---
+++ FDP/fdp.py
@@ -0,0 +1,30 @@
+#
+# Minimalistic FAIR Data Point Service
+# with the following endpoints:
+#    /
+#    /catalog
+#    /catalog/{dataset}
+#
+
+from bottle import Bottle, run, static_file
+
+app = Bottle()
+
+@app.route('/')
+def FAIRDataPointMetadata():
+   return static_file('fairdatapoint.ttl', root='metadata')
+
+@app.route('/catalog')
+def CatalogMetadata():
+   return static_file('catalog.ttl', root='metadata')
+
+@app.route('/catalog/<dataset>')
+def DatasetMetadata(dataset):
+   filename = '{dataset}.ttl'.format(dataset=dataset)
+   return static_file(filename, root='metadata')
+
+@app.get('/doc/<filename:re:.*>') # Swagger documentation '/index.html'
+def html(filename):
+   return static_file(filename, root='doc/')
+
+run(app, host='localhost', port='8080', debug=True)
