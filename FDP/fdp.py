022a365985f5b4d7808e9fbc3ce7f0de8189fb05
arnikz <arni@Arnolds-MacBook-Air.local>
2016-01-06 17:00:00 +0100

    Added LICENSE.

--- FDP/fdp.py
+++ FDP/fdp.py
@@ -1,11 +1,35 @@
 #
-# Minimalist FAIR Data Point (FDP) Service with the following endpoints:
-#  /
-#  /catalog
-#  /catalog/{dataset} # with 'breedb' example dataset
+# Copyright 2015 Netherlands eScience Center in collaboration with
+# Dutch Techcenter for Life Sciences.
 #
-# The service returns metadata using Data Catalog Vocabulary (DCAT).
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#################################################################################
+#
+# Minimalist FAIR Data Point (FDP) Metadata Service with the following endpoints:
+#
+#  /                  = FDP metadata
+#  /catalog           = catalog-level metadata
+#  /catalog/{dataset} = dataset-level metadata with 'breedb' as an example
+#
+# The FDP service returns metadata using Data Catalog Vocabulary (DCAT).
+#
+#################################################################################
+
+__author__  = 'Arnold Kuzniar'
+__version__ = '0.1'
+__status__  = 'Prototype'
+__license__ = 'Apache Lincense, Version 2.0'
 
 from bottle import Bottle, run, static_file
 
