31b5246b4df0128678ad3cb60a3ea1f56d154deb
rajaram5 <rr.kaliyaperumal@gmail.com>
2016-01-14 10:05:08 +0100

    Examples turtle files added

--- src/test/java/nl/dtls/fairdatapoint/domain/StoreManagerImplTest.java
+++ src/test/java/nl/dtls/fairdatapoint/domain/StoreManagerImplTest.java
@@ -5,19 +5,16 @@
  */
 package nl.dtls.fairdatapoint.domain;
 
-import java.io.IOException;
 import nl.dtls.fairdatapoint.api.config.RestApiConfiguration;
-import nl.dtls.fairdatapoint.utils.ExampleTurtle;
+import nl.dtls.fairdatapoint.utils.ExampleTurtleFiles;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openrdf.model.Statement;
 import org.openrdf.repository.Repository;
-import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.RepositoryResult;
 import org.openrdf.repository.sail.SailRepository;
-import org.openrdf.rio.RDFParseException;
 import org.openrdf.sail.Sail;
 import org.openrdf.sail.memory.MemoryStore;
 import org.springframework.test.context.ContextConfiguration;
@@ -43,7 +40,8 @@ public class StoreManagerImplTest {
         Sail store = new MemoryStore();
         Repository repository = new SailRepository(store);    
         StoreManager testStoreManager = new StoreManagerImpl(repository); 
-        uploadRDF(repository);  
+        ExampleTurtleFiles.storeTurtleFileToTripleStore(repository, 
+                ExampleTurtleFiles.FDP_METADATA, null);  
         String uri = "http://semlab1.liacs.nl:8080/fdp";             
         RepositoryResult<Statement> statements = 
                 testStoreManager.retrieveResource(uri); 
@@ -65,7 +63,8 @@ public class StoreManagerImplTest {
         Sail store = new MemoryStore();
         Repository repository = new SailRepository(store);    
         StoreManager testStoreManager = new StoreManagerImpl(repository); 
-        uploadRDF(repository);  
+        ExampleTurtleFiles.storeTurtleFileToTripleStore(repository, 
+                ExampleTurtleFiles.FDP_METADATA, null); 
         String uri = "http://semlab1.liacs.nl:8080/dummy";             
         RepositoryResult<Statement> statements = 
                 testStoreManager.retrieveResource(uri); 
@@ -80,25 +79,7 @@ public class StoreManagerImplTest {
     }
     
     
-    private void uploadRDF (Repository repository) {
-        RepositoryConnection conn = null;        
-        try {
-            conn = repository.getConnection();
-            conn.add(ExampleTurtle.getTurtleAsFile(ExampleTurtle.FDP_METADATA), 
-                    null, ExampleTurtle.FILES_RDF_FORMAT);                   
-        }
-        catch (RepositoryException | IOException | RDFParseException e) {
-        }
-        finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            }
-            catch (Exception e) {
-            }
-        }
-    }
+    
     
     
 }
