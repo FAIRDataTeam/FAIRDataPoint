a14546bf118479c3ca7e3c222c8c967c5290d749
rajaram5 <rr.kaliyaperumal@gmail.com>
2016-02-15 13:40:04 +0100

    Some docs added

--- fdp-api/java/FairDataPoint/src/main/java/nl/dtls/fairdatapoint/api/config/ApplicationInitializer.java
+++ fdp-api/java/FairDataPoint/src/main/java/nl/dtls/fairdatapoint/api/config/ApplicationInitializer.java
@@ -3,7 +3,8 @@ package nl.dtls.fairdatapoint.api.config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 /**
- *
+ * Application config (Similar to web.xml)
+ * 
  * @author Rajaram Kaliyaperumal
  * @since 2015-11-19
  * @version 0.1
