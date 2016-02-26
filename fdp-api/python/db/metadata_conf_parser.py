f9c234f64dae1eb52e17938157db3a04eb05315a
arnikz <a.kuzniar@esciencecenter.nl>
2016-02-26 16:42:43 +0100

    Use camel case naming convention for classes and methods.

--- fdp-api/python/db/metadata_conf_parser.py
+++ fdp-api/python/db/metadata_conf_parser.py
@@ -4,7 +4,7 @@ _CORE_META   = ['title','publisher','version','issued','modified']
 _REQUIRED_META = dict(fdp          = _CORE_META + ['fdp_id','catalog_id'],
                       catalog      = _CORE_META + ['dataset_id','theme_taxonomy'],
                       dataset      = _CORE_META + ['distribution_id','theme'],
-                      distribution = _CORE_META + ['access_url|download_url','media_type'])
+                      distribution = _CORE_META + ['access_url|download_url','media_type','license'])
 
 class FAIRConfigParser(object):
    def __init__(self):
@@ -14,21 +14,21 @@ class FAIRConfigParser(object):
 
 
    @staticmethod
-   def msg_section_not_found(section):
+   def errorSectionNotFound(section):
       return "Section '%s' is not found." % section
 
 
    @staticmethod
-   def msg_field_not_found(field, section):
+   def errorFieldNotFound(field, section):
       return "Field '%s' is not found in section '%s'." % (field, section)
 
    
    @staticmethod
-   def msg_reference_not_found(section, field, ref_section_by_field):
+   def errorReferenceNotFound(section, field, ref_section_by_field):
       return "{f}(s) in the '{s}' section is not referenced in the '{r}/<{f}>' section header(s) or vice versa.".format(f=field, r=ref_section_by_field, s=section)
 
    @staticmethod
-   def msg_nonuniq_resource_id(id):
+   def errorResourceIdNotUnique(id):
       return "Resource ID '%s' is not unique." % id
 
 
@@ -45,24 +45,24 @@ class FAIRConfigParser(object):
       self._validate()
 
 
-   def get_all(self):
+   def getMetadata(self):
       return self._metadata
 
 
-   def get_section_headers(self):
-      return self.get_all().keys()
+   def getSectionHeaders(self):
+      return self.getMetadata().keys()
 
 
-   def get_fields(self, section):
+   def getFields(self, section):
       return self._metadata[section]
 
 
-   def get_items(self, section, field):
+   def getItems(self, section, field):
       return self._metadata[section][field]
 
 
    def _validate(self):
-      section_headers = self.get_section_headers()
+      section_headers = self.getSectionHeaders()
       sections = dict((section,[]) for section in _REQUIRED_META.keys())
       uniq_resource_ids = dict()
 
@@ -86,44 +86,44 @@ class FAIRConfigParser(object):
                sections[section].append(resource_id)
 
       for section,resource in sections.items():
-         assert(resource), self.msg_section_not_found(section)
+         assert(resource), self.errorSectionNotFound(section)
 
       # check mandatory fields and referenced sections
       for section in section_headers:
          for field in _REQUIRED_META[section.split('/')[0]]:
-            fields = self.get_fields(section)
+            fields = self.getFields(section)
          
             if '|' in field: # distribution has two alternatives: access_url|download_url
                a, b = field.split('|')
-               assert(a in fields or b in fields), self.msg_field_not_found(field, section)
+               assert(a in fields or b in fields), self.errorFieldNotFound(field, section)
             else:
-               assert(field in fields), self.msg_field_not_found(field, section)
+               assert(field in fields), self.errorFieldNotFound(field, section)
 
             # resource IDs must be unique
             if field in [fdp_id, cat_id, dat_id, dist_id]:
-               for resource_id in _arrfy(self.get_items(section, field)):
-                  assert(resource_id not in uniq_resource_ids), self.msg_nonuniq_resource_id(resource_id)
+               for resource_id in _arrfy(self.getItems(section, field)):
+                  assert(resource_id not in uniq_resource_ids), self.errorResourceIdNotUnique(resource_id)
                   uniq_resource_ids[resource_id] = None
 
          if fdp in section:
-            ids1, ids2 = _arrfy(self.get_items(section, cat_id)), sections[cat]
-            assert(ids1 == ids2), self.msg_reference_not_found(fdp, cat_id, cat)
+            ids1, ids2 = _arrfy(self.getItems(section, cat_id)), sections[cat]
+            assert(ids1 == ids2), self.errorReferenceNotFound(fdp, cat_id, cat)
 
          if cat in section:
-            ids1, ids2 = _arrfy(self.get_items(section, dat_id)), sections[dat]
-            assert(ids1 == ids2), self.msg_reference_not_found(cat, dat_id, dat)
+            ids1, ids2 = _arrfy(self.getItems(section, dat_id)), sections[dat]
+            assert(ids1 == ids2), self.errorReferenceNotFound(cat, dat_id, dat)
 
          if dat in section:
-            ids1, ids2 = _arrfy(self.get_items(section, dist_id)), sections[dist]
-            assert(ids1 == ids2), self.msg_reference_not_found(dat, dist_id, dist)
+            ids1, ids2 = _arrfy(self.getItems(section, dist_id)), sections[dist]
+            assert(ids1 == ids2), self.errorReferenceNotFound(dat, dist_id, dist)
 
 
 filename = 'metadata.ini'
 parser = FAIRConfigParser()
 parser.read(filename)
 
-print parser.get_all()
-#print parser.get_section_headers()
-#print parser.get_fields('dataset/breedb')
-#print parser.get_items('fdp', 'fdp_id')
+print parser.getMetadata()
+#print parser.getSectionHeaders()
+#print parser.getFields('dataset/breedb')
+#print parser.getItems('fdp', 'fdp_id')
 
