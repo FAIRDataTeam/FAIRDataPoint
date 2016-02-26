18e07c057f0da131766992e93fbe505d5b0559bd
arnikz <a.kuzniar@esciencecenter.nl>
2016-02-26 15:54:31 +0100

    Check the uniqueness of resource IDs; minor changes to metadata config reader.

--- fdp-api/python/db/metadata_conf_parser.py
+++ fdp-api/python/db/metadata_conf_parser.py
@@ -15,17 +15,21 @@ class FAIRConfigParser(object):
 
    @staticmethod
    def msg_section_not_found(section):
-      return "Section '%s' not found." % section
+      return "Section '%s' is not found." % section
 
 
    @staticmethod
    def msg_field_not_found(field, section):
-      return "Field '%s' not found in section '%s'." % (field, section)
+      return "Field '%s' is not found in section '%s'." % (field, section)
 
    
    @staticmethod
    def msg_reference_not_found(section, field, ref_section_by_field):
-      return "{f}(s) in the '{s}' section not referenced in the '{r}/<{f}>' section header(s) or vice versa.".format(f=field, r=ref_section_by_field, s=section)
+      return "{f}(s) in the '{s}' section is not referenced in the '{r}/<{f}>' section header(s) or vice versa.".format(f=field, r=ref_section_by_field, s=section)
+
+   @staticmethod
+   def msg_nonuniq_resource_id(id):
+      return "Resource ID '%s' is not unique." % id
 
 
    def read(self, filename):
@@ -58,18 +62,20 @@ class FAIRConfigParser(object):
 
 
    def _validate(self):
-      # check the presence of mandatory sections and fields in config file
       section_headers = self.get_section_headers()
       sections = dict((section,[]) for section in _REQUIRED_META.keys())
+      uniq_resource_ids = dict()
+
       sfx = '_id'
       fdp, cat, dat, dist = 'fdp', 'catalog', 'dataset', 'distribution'
-      cat_id, dat_id, dist_id = cat + sfx, dat + sfx, dist + sfx
+      fdp_id, cat_id, dat_id, dist_id = fdp + sfx, cat + sfx, dat + sfx, dist + sfx
 
       def _arrfy(item):
          if isinstance(item, str):
             return [item]
          return item
 
+      # check mandatory sections
       for sh in section_headers:
          if sh in sections:
             sections[sh] = True
@@ -82,6 +88,7 @@ class FAIRConfigParser(object):
       for section,resource in sections.items():
          assert(resource), self.msg_section_not_found(section)
 
+      # check mandatory fields and referenced sections
       for section in section_headers:
          for field in _REQUIRED_META[section.split('/')[0]]:
             fields = self.get_fields(section)
@@ -92,17 +99,23 @@ class FAIRConfigParser(object):
             else:
                assert(field in fields), self.msg_field_not_found(field, section)
 
+            # resource IDs must be unique
+            if field in [fdp_id, cat_id, dat_id, dist_id]:
+               for resource_id in _arrfy(self.get_items(section, field)):
+                  assert(resource_id not in uniq_resource_ids), self.msg_nonuniq_resource_id(resource_id)
+                  uniq_resource_ids[resource_id] = None
+
          if fdp in section:
-            items = self.get_items(section, cat_id)
-            assert(_arrfy(items) == sections[cat]), self.msg_reference_not_found(fdp, cat_id, cat)
+            ids1, ids2 = _arrfy(self.get_items(section, cat_id)), sections[cat]
+            assert(ids1 == ids2), self.msg_reference_not_found(fdp, cat_id, cat)
 
          if cat in section:
-            items = self.get_items(section, dat_id)
-            assert(_arrfy(items) == sections[dat]), self.msg_reference_not_found(cat, dat_id, dat)
+            ids1, ids2 = _arrfy(self.get_items(section, dat_id)), sections[dat]
+            assert(ids1 == ids2), self.msg_reference_not_found(cat, dat_id, dat)
 
          if dat in section:
-            items = self.get_items(section, dist_id)
-            assert(_arrfy(items) == sections[dist]), self.msg_reference_not_found(dat, dist_id, dist)
+            ids1, ids2 = _arrfy(self.get_items(section, dist_id)), sections[dist]
+            assert(ids1 == ids2), self.msg_reference_not_found(dat, dist_id, dist)
 
 
 filename = 'metadata.ini'
