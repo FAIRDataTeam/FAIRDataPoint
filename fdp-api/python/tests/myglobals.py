60c19e0cc7adb65a1ce430006536f212862b7b05
arnikz <a.kuzniar@esciencecenter.nl>
2016-03-04 21:31:44 +0100

    Add smoke test: compare FDP responses with reference metadata in all supported serializations.

--- fdp-api/python/tests/myglobals.py
+++ fdp-api/python/tests/myglobals.py
@@ -1,5 +1,7 @@
 DUMP_DIR = 'metadata'
 BASE_URL = 'http://127.0.0.1:8080'
+
+# example catalog, dataset and distributions
 URL_PATHS = [ 'fdp',
               'catalog/catalog-01',
               'dataset/breedb',
@@ -11,4 +13,5 @@ MIME_TYPES = { 'text/turtle'           : 'ttl',
                'application/n-triples' : 'n3',
                'application/rdf+xml'   : 'rdf',
                'application/ld+json'   : 'jsonld' }
+
 
