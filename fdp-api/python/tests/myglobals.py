cb77b02df6b964eefc28f4023ee25075a094044e
arnikz <a.kuzniar@esciencecenter.nl>
2016-03-04 18:23:02 +0100

    Add helper module.

---
+++ fdp-api/python/tests/myglobals.py
@@ -0,0 +1,14 @@
+DUMP_DIR = 'metadata'
+BASE_URL = 'http://127.0.0.1:8080'
+URL_PATHS = [ 'fdp',
+              'catalog/catalog-01',
+              'dataset/breedb',
+              'distribution/breedb-sparql',
+              'distribution/breedb-sqldump' ]
+
+# lookup table: MIME type - file extension pairs
+MIME_TYPES = { 'text/turtle'           : 'ttl',
+               'application/n-triples' : 'n3',
+               'application/rdf+xml'   : 'rdf',
+               'application/ld+json'   : 'jsonld' }
+
