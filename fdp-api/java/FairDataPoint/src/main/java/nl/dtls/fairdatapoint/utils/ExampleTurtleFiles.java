c17d50b15a52e7abc47540db2a2bec205263c580
rajaram5 <r.kaliyaperumal@lumc.nl>
2016-05-23 13:51:35 +0200

    Unused methods and variables are removed, some variables renamed

--- fdp-api/java/FairDataPoint/src/main/java/nl/dtls/fairdatapoint/utils/ExampleTurtleFiles.java
+++ fdp-api/java/FairDataPoint/src/main/java/nl/dtls/fairdatapoint/utils/ExampleTurtleFiles.java
@@ -6,13 +6,11 @@
 package nl.dtls.fairdatapoint.utils;
 
 import com.google.common.base.Charsets;
-import com.google.common.collect.ImmutableList;
 import com.google.common.io.PatternFilenameFilter;
 import com.google.common.io.Resources;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,19 +26,11 @@ import org.apache.logging.log4j.Logger;
  * 
  * @author Rajaram Kaliyaperumal
  * @since 2015-12-01
- * @version 0.1
+ * @version 0.2
  */
 public class ExampleTurtleFiles {
     
-    public static final String FDP_METADATA = "dtl-fdp.ttl";
-    public static final ImmutableList<String>  CATALOG_METADATA = 
-        ImmutableList.of("textmining-catalog.ttl");
-    public static final ImmutableList<String> DATASET_METADATA = 
-            ImmutableList.of("disgenet.ttl", "gda-lumc.ttl");
-    public static final ImmutableList<String> DATASET_DISTRIBUTIONS = 
-        ImmutableList.of("disgenet-html-page.ttl" , 
-                "disgenet-nanopubs-gzip.ttl", "disgenet-textfile-gzip.ttl", 
-                "gda-lumc-textfile.ttl", "gda-lumc-sparql.ttl");
+    public static final String EXAMPLE_FDP_METADATA_FILE = "dtl-fdp.ttl";
     public static final String EXAMPLE_CATALOG_ID = "textmining";
     public static final String EXAMPLE_DATASET_ID = 
             "gene-disease-association_lumc";
@@ -48,7 +38,8 @@ public class ExampleTurtleFiles {
     public static final RDFFormat FILES_RDF_FORMAT = RDFFormat.TURTLE;
     private final static Logger LOGGER = LogManager.getLogger(
             ExampleTurtleFiles.class.getName());
-    public final static String BASE_URI = "http://semlab1.liacs.nl:8080/";
+    public final static String EXAMPLE_FILES_BASE_URI = 
+            "http://semlab1.liacs.nl:8080/";
     public final static String FDP_URI = "http://semlab1.liacs.nl:8080/fdp";
     
     public static String getTurtleAsString(String fileName)  {        
@@ -60,20 +51,7 @@ public class ExampleTurtleFiles {
             LOGGER.error("Error getting turle file",ex);          
         }        
         return content;
-    }
-    
-    public static File getTurtleAsFile(String fileName)  {        
-        File npFile = null;       
-        URL fileURL = ExampleTurtleFiles.class.getResource(fileName);
-        try {
-            
-            npFile = new File(fileURL.toURI());        
-        } catch (URISyntaxException ex) {
-            LOGGER.error("Error getting turle file",ex);        
-        }        
-        return npFile;
-    }
-    
+    }    
     /**
      * Method to get file names from the util package
      * 
@@ -82,9 +60,10 @@ public class ExampleTurtleFiles {
     public static List<String> getExampleTurtleFileNames () { 
         
         List<String> fileNames = new ArrayList();    
-        URL fdpFileURL = ExampleTurtleFiles.class.getResource(FDP_METADATA);
+        URL fdpFileURL = ExampleTurtleFiles.class.getResource(
+                EXAMPLE_FDP_METADATA_FILE);
         String sourceFileURI = fdpFileURL.getPath();
-        sourceFileURI = sourceFileURI.replace(FDP_METADATA, "");
+        sourceFileURI = sourceFileURI.replace(EXAMPLE_FDP_METADATA_FILE, "");
         // Matches only turtle files
         Pattern pattern = Pattern.compile("^.*.ttl");    
         FilenameFilter filterByExtension = new PatternFilenameFilter(pattern);
