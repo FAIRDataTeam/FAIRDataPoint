2cc8932395b93a45daac66b739469dc73839ba22
kburger <burger.github@gmail.com>
2016-09-20 11:40:58 +0200

    spring security skeleton

---
+++ src/main/java/nl/dtls/fairdatapoint/api/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package nl.dtls.fairdatapoint.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST)
+                .hasIpAddress("127.0.0.1");
+    }
+}
---
+++ src/main/java/nl/dtls/fairdatapoint/api/config/SecurityInitializer.java
@@ -0,0 +1,9 @@
+package nl.dtls.fairdatapoint.api.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Spring security initializer.
+ */
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
