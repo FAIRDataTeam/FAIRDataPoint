efc9313236c0367b899a47c32f4e11f017743293
rkaliyaperumal <r.kaliyaperumal@lumc.nl>
2016-09-20 16:41:29 +0200

    SecurityConfig to limit POST calls to localhost

--- src/main/java/nl/dtls/fairdatapoint/api/config/SecurityConfig.java
+++ src/main/java/nl/dtls/fairdatapoint/api/config/SecurityConfig.java
@@ -6,13 +6,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * SecurityConfig to limit POST calls to localhost
+ * 
+ * @author Kees Burger
+ * @since 2016-09-20
+ * @version 0.1
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.POST)
-                .hasIpAddress("127.0.0.1");
+         http.authorizeRequests()
+                .antMatchers(HttpMethod.POST).hasIpAddress("127.0.0.1")
+                .anyRequest().permitAll()
+            .and()
+            .csrf().disable();
     }
 }
