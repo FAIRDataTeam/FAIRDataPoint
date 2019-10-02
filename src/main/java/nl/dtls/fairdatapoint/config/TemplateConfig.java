dc71a98d3a6859f22b9bfe67908dff2f3e6a3f83
Jan Slifka <jan.slifka@gmail.com>
2019-10-02 13:27:52 +0200

    Add simple resolving of visible name from urls

--- src/main/java/nl/dtls/fairdatapoint/config/TemplateConfig.java
+++ src/main/java/nl/dtls/fairdatapoint/config/TemplateConfig.java
@@ -22,9 +22,10 @@
  */
 package nl.dtls.fairdatapoint.config;
 
-import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
 import com.github.jknack.handlebars.springmvc.HandlebarsViewResolver;
 import nl.dtls.fairdatapoint.Profiles;
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Literal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +38,10 @@ import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 
+
+/**
+ * This class defines and registers helpers for Handlebars templates.
+ */
 @Configuration
 @Profile(value = Profiles.NON_TESTING)
 public class TemplateConfig {
@@ -44,7 +49,90 @@ public class TemplateConfig {
     @Autowired
     private HandlebarsViewResolver handlebarsViewResolver;
 
-    private static Date stringToDate(String string) {
+    @PostConstruct
+    public void registerHelper() {
+        handlebarsViewResolver.registerHelper("length", TemplateConfig::length);
+        handlebarsViewResolver.registerHelper("literal", TemplateConfig::literal);
+        handlebarsViewResolver.registerHelper("simplifyUrl", TemplateConfig::simplifyUrl);
+        handlebarsViewResolver.registerHelper("stringToDate", TemplateConfig::stringToDate);
+        handlebarsViewResolver.registerHelper("trim", TemplateConfig::trim);
+    }
+
+    /**
+     * Helper to return a length of the given collection.
+     *
+     * Usage in template:
+     *
+     * {{length myCollection}}
+     *
+     * @param collection Given collection
+     * @param options    Options are not used for this helper
+     * @return Length of the collection
+     */
+    private static Integer length(Collection collection, Options options) {
+        if (collection != null) {
+            return collection.size();
+        }
+        return 0;
+    }
+
+    /**
+     * Helper to get a label of the given literal.
+     *
+     * Usage in template:
+     *
+     * {{literal myLiteral}}
+     *
+     * @param literal Given literal
+     * @param options Options are not used for this helper
+     * @return Literal label or empty string if the literal is null
+     */
+    private static String literal(Literal literal, Options options) {
+        if (literal != null) {
+            return literal.getLabel();
+        }
+        return "";
+    }
+
+    /**
+     * Helper that returns only the last part of the given URL to make it more readable. E.g.:
+     *
+     * http://rdflicense.appspot.com/rdflicense/cc-by-nc-nd3.0 -> cc-by-nc-nd3.0
+     *
+     * Usage in template:
+     *
+     * {{simplifyUrl myUrl}}
+     *
+     * @param iri     Given URL
+     * @param options Options are not used for this helper
+     * @return Last part of the URL or empty string
+     */
+    private static String simplifyUrl(IRI iri, Options options) {
+        if (iri != null) {
+            String[] parts = iri.toString().split("/");
+            if (parts.length > 0) {
+                return parts[parts.length - 1];
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Helper to convert a datetime string used in FDP to actual Date object.
+     *
+     * Usage in template:
+     *
+     * {{stringToDate myString}}
+     *
+     * This can be easily chained with Handlebars dateFormat helper:
+     *
+     * {{dateFormat (stringToDate myString) "dd-MM-YYYY"}}
+     *
+     * @param string  String with datetime format yyyy-MM-dd'T'HH:mm:ss.SSSZ
+     * @param options Options are not used for this helper
+     * @return Date object created from the given string
+     */
+    private static Date stringToDate(String string, Options options) {
         try {
             final DateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
             return format.parse(string);
@@ -53,23 +141,26 @@ public class TemplateConfig {
         }
     }
 
-    private static String trim(String string, Integer limit) {
+    /**
+     * Helper to trim given string to not exceed given limit. If the given string is longer than the
+     * limit, it is trimmed and "..." is added at the end. If the string is shorter or no limit is
+     * given, the original string is returned.
+     *
+     * Usage in template:
+     *
+     * {{trim myString limit=180}}
+     *
+     * @param string  Given string
+     * @param options Options can contain
+     * @return Trimmed string
+     */
+    private static String trim(String string, Options options) {
+        Integer limit = options.hash("limit");
+
         if (limit == null || string.length() <= limit) {
             return string;
         }
 
         return string.substring(0, limit) + "...";
-    }
-
-    @PostConstruct
-    public void registerHelper() {
-        handlebarsViewResolver.registerHelper("literal",
-                (Helper<Literal>) (literal, options) -> literal.getLabel());
-        handlebarsViewResolver.registerHelper("stringToDate",
-                (Helper<String>) (string, options) -> stringToDate(string));
-        handlebarsViewResolver.registerHelper("length",
-                (Helper<Collection>) (collection, options) -> collection.size());
-        handlebarsViewResolver.registerHelper("trim",
-                (Helper<String>) (string, options) -> trim(string, options.hash("limit")));
     }
 }
