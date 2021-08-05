9abfee24cc1c928cff5ddb8e3e23bd270b5cb1d8
kburger <6997485+kburger@users.noreply.github.com>
2021-08-05 10:49:01 +0200

    Update ProfileService.java
    Fixed the profile namespace (`/` vs `#`, conforming to https://www.w3.org/TR/dx-prof/#namespaces)

--- src/main/java/nl/dtls/fairdatapoint/service/profile/ProfileService.java
+++ src/main/java/nl/dtls/fairdatapoint/service/profile/ProfileService.java
@@ -47,7 +47,7 @@ import static nl.dtls.fairdatapoint.util.ValueFactoryHelper.*;
 @Service
 public class ProfileService {
 
-    private static final String PROFILE_PREFIX = "http://www.w3.org/ns/dx/prof";
+    private static final String PROFILE_PREFIX = "http://www.w3.org/ns/dx/prof/";
 
     @Autowired
     @Qualifier("persistentUrl")
@@ -61,9 +61,9 @@ public class ProfileService {
 
     private Model getProfileForResourceDefinition(ResourceDefinition rd, IRI uri) {
         Model profile = new LinkedHashModel();
-        profile.add(uri, RDF.TYPE, i(format("%s#Profile", PROFILE_PREFIX)));
+        profile.add(uri, RDF.TYPE, i(format("%sProfile", PROFILE_PREFIX)));
         profile.add(uri, RDFS.LABEL, l(format("%s Profile", rd.getName())));
-        profile.add(uri, i(format("%s#isProfileOf", PROFILE_PREFIX)), i(format("%s/profile/core",
+        profile.add(uri, i(format("%sisProfileOf", PROFILE_PREFIX)), i(format("%s/profile/core",
                 persistentUrl)));
         rd.getShapeUuids().forEach(shapeUuid -> shapeRepository.findByUuid(shapeUuid).map(shape -> {
             ModelBuilder modelBuilder = new ModelBuilder();
@@ -72,11 +72,11 @@ public class ProfileService {
             modelBuilder.add(RDF.TYPE, i(format("%s#ResourceDescriptor", PROFILE_PREFIX)));
             modelBuilder.add(DCTERMS.FORMAT, i("https://w3id.org/mediatype/text/turtle"));
             modelBuilder.add(DCTERMS.CONFORMS_TO, i("https://www.w3.org/TR/shacl/"));
-            modelBuilder.add(i(format("%s#hasRole", PROFILE_PREFIX)), i(format("%s/role#Validation",
+            modelBuilder.add(i(format("%shasRole", PROFILE_PREFIX)), i(format("%s/role/Validation",
                     PROFILE_PREFIX)));
-            modelBuilder.add(i(format("%s#hasArtifact", PROFILE_PREFIX)), i(format("%s/shapes/%s",
+            modelBuilder.add(i(format("%shasArtifact", PROFILE_PREFIX)), i(format("%s/shapes/%s",
                     persistentUrl, shapeUuid)));
-            profile.add(uri, i(format("%s#hasResource", PROFILE_PREFIX)), resource);
+            profile.add(uri, i(format("%shasResource", PROFILE_PREFIX)), resource);
             profile.addAll(new ArrayList<>(modelBuilder.build()));
             return null;
         }));
