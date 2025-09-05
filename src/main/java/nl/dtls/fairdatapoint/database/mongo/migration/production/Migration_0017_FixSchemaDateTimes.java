b55fb1181037b0dc0fa35fc60cc77245535c6f2b
Dennis <29799340+dennisvang@users.noreply.github.com>
2025-09-05 14:14:37 +0200

    Consistent datatype and editor for date/datetime (#765)
    * consistently use dash:DateTimePickerEditor for xsd:dateTime fields
       - dct:issued
       - dct:modified
    * change fdp startDate and endDate datatypes to xsd:date, consistent with the fdp specs
    * add *.ttl files with updated SHACL shapes for fdp, dataset, distribution, to use in new migration (0017)
    * add new migration class (0017)
    * add readme to clarify the role of migration resources
    
    note that the `repository` shape from migration 0005 also has some date fields, but this shape was removed again in migration 0011

---
+++ src/main/java/nl/dtls/fairdatapoint/database/mongo/migration/production/Migration_0017_FixSchemaDateTimes.java
@@ -0,0 +1,92 @@
+/**
+ * The MIT License
+ * Copyright © 2017 DTL
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package nl.dtls.fairdatapoint.database.mongo.migration.production;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.SneakyThrows;
+import nl.dtls.fairdatapoint.Profiles;
+import nl.dtls.fairdatapoint.util.KnownUUIDs;
+import org.bson.Document;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import static java.lang.String.format;
+import static nl.dtls.fairdatapoint.util.ResourceReader.loadClassResource;
+
+/**
+ * This migration updates the default metadata schemas defining the `fdp`, `dataset`, and `distribution` shapes.
+ * This is done by copying the content from `resources/.../migration/production/0017_shape-*.ttl` files into the
+ * corresponding `definition` fields.
+ * Changes with respect to migration 0016:
+ * - changed `xsd:dateTime` to `xsd:date` for fdp `startDate` and `endDate` (to comply with the spec)
+ * - changed `dash:editor` for `issued` and `modified` from `DatePickerEditor` to `DateTimePickerEditor`,
+ *   in `dataset` and `distribution`, for consistency with the `xsd:dateTime` datatype for those fields
+ */
+@ChangeUnit(
+        id = "Migration_0017_FixSchemaDateTimes",
+        order = "0017",
+        author = "migrationBot"
+)
+@Profile(Profiles.PRODUCTION)
+public class Migration_0017_FixSchemaDateTimes {
+    private static final String FIELD_VER_UUID = "versionUuid";
+    private static final String FIELD_LATEST = "latest";
+    private static final String FIELD_DEF = "definition";
+    private static final String COL_SCHEMAS = "metadataSchema";
+
+    private final MongoTemplate database;
+
+    public Migration_0017_FixSchemaDateTimes(MongoTemplate template) {
+        this.database = template;
+    }
+
+    @Execution
+    public void run() {
+        updateSchema(KnownUUIDs.SCHEMA_V1_DATASET_UUID, "dataset");
+        updateSchema(KnownUUIDs.SCHEMA_V1_DISTRIBUTION_UUID, "distribution");
+        updateSchema(KnownUUIDs.SCHEMA_V1_FDP_UUID, "fdp");
+    }
+
+    @SneakyThrows
+    private void updateSchema(String versionUuid, String name) {
+        final MongoCollection<Document> schemasCol = database.getCollection(COL_SCHEMAS);
+        final String definition = loadClassResource(format("0017_shape-%s.ttl", name), getClass());
+        schemasCol.updateOne(
+                Filters.and(
+                        Filters.eq(FIELD_VER_UUID, versionUuid),
+                        Filters.eq(FIELD_LATEST, true)
+                ),
+                Updates.set(FIELD_DEF, definition)
+        );
+    }
+
+    @RollbackExecution
+    public void rollback() {
+        // Rollback is not possible
+    }
+}
