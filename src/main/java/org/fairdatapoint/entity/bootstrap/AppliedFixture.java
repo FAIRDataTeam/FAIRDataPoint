645afd72aed780e19e537a329930cfea8698ed52
dennisvang <29799340+dennisvang@users.noreply.github.com>
2025-10-21 18:00:29 +0200

    add AppliedFixture entity

---
+++ src/main/java/org/fairdatapoint/entity/bootstrap/AppliedFixture.java
@@ -0,0 +1,24 @@
+package org.fairdatapoint.entity.bootstrap;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.fairdatapoint.entity.base.BaseEntity;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppliedFixture extends BaseEntity {
+
+    @NotNull
+    @Column(unique = true)
+    private String filename;
+
+}
