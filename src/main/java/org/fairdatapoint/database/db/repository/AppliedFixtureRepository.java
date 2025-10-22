3fed0c386b5cc57700046c6916a130a465e91173
dennisvang <29799340+dennisvang@users.noreply.github.com>
2025-10-22 11:04:09 +0200

    explicit @Repository annotation
     for consistency

--- src/main/java/org/fairdatapoint/database/db/repository/AppliedFixtureRepository.java
+++ src/main/java/org/fairdatapoint/database/db/repository/AppliedFixtureRepository.java
@@ -2,9 +2,11 @@ package org.fairdatapoint.database.db.repository;
 
 import org.fairdatapoint.database.db.repository.base.BaseRepository;
 import org.fairdatapoint.entity.bootstrap.AppliedFixture;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AppliedFixtureRepository extends BaseRepository<AppliedFixture> {
     Optional<AppliedFixture> findByFilename(String filename);
 }
