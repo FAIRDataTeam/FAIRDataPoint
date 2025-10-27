a942b553b1a9db31d9d999c57f49b6b710d72b13
dennisvang <29799340+dennisvang@users.noreply.github.com>
2025-10-27 11:29:04 +0100

    mark RdfDevelopmentMigrationRunner deprecated
    Keep it around for reference, but remove before merging into develop.

--- src/main/java/org/fairdatapoint/database/rdf/migration/RdfDevelopmentMigrationRunner.java
+++ src/main/java/org/fairdatapoint/database/rdf/migration/RdfDevelopmentMigrationRunner.java
@@ -35,6 +35,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+/**
+ * Loads initial RDF data for development.
+ * TODO: remove all classes and files related to RDF migration, in favor of the fixtures-based approach.
+ * @deprecated use RDF fixtures instead, see {@link org.fairdatapoint.service.bootstrap bootstrap package}
+ */
+@Deprecated(forRemoval = true, since = "1")
 @Service
 @Profile(Profiles.NON_PRODUCTION)
 public class RdfDevelopmentMigrationRunner {
