57ceddacae636baa6a6c97a72efed96b100dabe1
dennisvang <29799340+dennisvang@users.noreply.github.com>
2025-12-01 15:25:30 +0100

    update BootstrapConfig javadoc

--- src/main/java/org/fairdatapoint/config/BootstrapConfig.java
+++ src/main/java/org/fairdatapoint/config/BootstrapConfig.java
@@ -44,17 +44,14 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * The {@code BootstrapConfig} class configures a repository populator to load initial data into the relational
+ * The {@code BootstrapConfig} class configures a repository populator that loads initial data into the relational
  * database, based on JSON fixture files.
- * Bootstrapping is disabled by default, and should only be enabled once, on the very first run of the application.
- * It can also be enabled on subsequent runs, but then it will overwrite any changes that may have been made by users.
- * To enable on the first run, set the env variable {@code BOOTSTRAP_ENABLED=true} on the command line, before running
- * the app.
- * When using e.g. docker compose, you can define {@code BOOTSTRAP_ENABLED: ${BOOTSTRAP_ENABLED:-false}} in the
- * {@code environment} section and then set up the stack by running {@code BOOTSTRAP_ENABLED=true docker compose up -d}.
- * The default fixtures are located in the {@code <project-root>/fixtures} directory.
+ * The default fixture files are located in the {@code <project-root>/fixtures} directory.
+ * Additional fixture directories can also be specified, using the {@code dbFixturesDirs} property.
+ * Fixture files are collected from all specified directories and are applied in lexicographic order.
+ * A FixtureHistory repository keeps track of fixture files that have been applied, so they are only applied once.
  * To add custom fixtures and/or override any of the default fixtures in a docker compose setup, we can bind-mount
- * individual fixture files.
+ * individual fixture files or entire directories.
  * For example: {@code ./my-fixtures/0100_user-accounts.json:/fdp/fixtures/0100_user-accounts.json:ro}
  * Note that bind-mounting the entire directory, instead of individual files, would hide all default files.
  */
