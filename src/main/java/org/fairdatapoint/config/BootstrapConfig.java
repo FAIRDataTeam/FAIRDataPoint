86225f268c8821835f908b107215cef059ee1951
dennisvang <29799340+dennisvang@users.noreply.github.com>
2025-12-09 17:55:17 +0100

    simplify resource location pattern for Windows compatibility
    the Path methods caused errors on windows if the location included ':' or '*' characters

--- src/main/java/org/fairdatapoint/config/BootstrapConfig.java
+++ src/main/java/org/fairdatapoint/config/BootstrapConfig.java
@@ -38,7 +38,6 @@ import org.springframework.data.repository.init.RepositoriesPopulatedEvent;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -78,11 +77,13 @@ public class BootstrapConfig {
                 log.info("Looking for db fixtures in the following locations: {}",
                         String.join(", ", this.bootstrap.getLocations()));
                 for (String location : this.bootstrap.getLocations()) {
-                    // Only look for JSON files. Notes:
-                    // - Path.of() removes trailing slashes, so it is safe to concatenate "/*.json".
-                    // - Path.of(location).resolve("*.json") could work on Unix but fails on Windows.
-                    final String pattern = location.endsWith(".json") ? location : Path.of(location) + "/*.json";
-                    resources.addAll(List.of(resourceResolver.getResources(pattern)));
+                    // Only look for JSON files
+                    String locationPattern = location;
+                    if (!locationPattern.endsWith(".json")) {
+                        // naive append may lead to redundant slashes, but the OS ignores those
+                        locationPattern += "/*.json";
+                    }
+                    resources.addAll(List.of(resourceResolver.getResources(locationPattern)));
                 }
                 // remove resources that have been applied already
                 final List<String> appliedFixtures = fixtureHistoryRepository.findAll().stream()
