3acae7f1914b2a9afedc95b2dd5e8e115bcdc15d
dennisvang <29799340+dennisvang@users.noreply.github.com>
2025-12-16 12:22:58 +0100

    remove redundant @ExtendWith annotations

--- src/test/java/org/fairdatapoint/BaseIntegrationTest.java
+++ src/test/java/org/fairdatapoint/BaseIntegrationTest.java
@@ -22,13 +22,10 @@
  */
 package org.fairdatapoint;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles(Profiles.TESTING)
 @SpringBootTest
 @DirtiesContext
