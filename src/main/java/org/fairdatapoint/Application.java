918d43625cc95fa69a366b658df79f8986d035f3
dennisvang <29799340+dennisvang@users.noreply.github.com>
2025-12-16 12:25:32 +0100

    remove redundant @EnableWebMvc annotation
    this is redundant because @SpringBootTest already configures MVC, and the global annotation prevented us from creating test slices using e.g. @DataJpaTest

--- src/main/java/org/fairdatapoint/Application.java
+++ src/main/java/org/fairdatapoint/Application.java
@@ -26,10 +26,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 @EnableAsync
 @ConfigurationPropertiesScan("org.fairdatapoint.config.*")
 public class Application {
