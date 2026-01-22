917e60bdf89a80a3075b975327a75a26674a7c21
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-01-22 14:03:24 +0100

    fix and simplify ResourceDefinitionServiceTest so it now reproduces #830

--- src/test/java/org/fairdatapoint/service/resource/ResourceDefinitionServiceTest.java
+++ src/test/java/org/fairdatapoint/service/resource/ResourceDefinitionServiceTest.java
@@ -24,13 +24,8 @@ package org.fairdatapoint.service.resource;
 
 import org.fairdatapoint.BaseIntegrationTest;
 import org.fairdatapoint.api.dto.resource.ResourceDefinitionChangeDTO;
-import org.fairdatapoint.api.dto.resource.ResourceDefinitionChildDTO;
-import org.fairdatapoint.api.dto.resource.ResourceDefinitionChildListViewDTO;
-import org.fairdatapoint.api.dto.resource.ResourceDefinitionChildListViewMetadataDTO;
 import org.fairdatapoint.entity.resource.ResourceDefinition;
-import org.fairdatapoint.entity.resource.ResourceDefinitionChild;
-import org.fairdatapoint.entity.resource.ResourceDefinitionChildMetadata;
-import org.junit.jupiter.api.BeforeEach;
+import org.fairdatapoint.util.KnownUUIDs;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
@@ -40,7 +35,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindException;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -53,6 +47,8 @@ public class ResourceDefinitionServiceTest extends BaseIntegrationTest {
 
     final ResourceDefinitionService resourceDefinitionService;
 
+    final ResourceDefinitionMapper resourceDefinitionMapper;
+
     final TestEntityManager testEntityManager;
 
     /**
@@ -63,109 +59,23 @@ public class ResourceDefinitionServiceTest extends BaseIntegrationTest {
     @Autowired
     public ResourceDefinitionServiceTest(
             ResourceDefinitionService resourceDefinitionService,
+            ResourceDefinitionMapper resourceDefinitionMapper,
             TestEntityManager testEntityManager
     ) {
         this.resourceDefinitionService = resourceDefinitionService;
+        this.resourceDefinitionMapper = resourceDefinitionMapper;
         this.testEntityManager = testEntityManager;
-    }
-
-    /**
-     * Creates ResourceDefinitionChangeDTO from existing ResourceDefinition
-     *
-     * @param uuid ResourceDefinition identifier
-     * @return Change DTO based on the ResourceDefinition
-     */
-    private ResourceDefinitionChangeDTO createResourceDefinitionChangeDTO(UUID uuid) {
-        ResourceDefinition resourceDefinition = testEntityManager.find(ResourceDefinition.class, uuid);
-        System.out.println(resourceDefinition.getChildren().size());  // TEMP
-        final ResourceDefinitionChangeDTO dto = new ResourceDefinitionChangeDTO();
-        dto.setName(resourceDefinition.getName());
-        dto.setUrlPrefix(resourceDefinition.getUrlPrefix());
-        dto.setMetadataSchemaUuids(resourceDefinition.getMetadataSchemaUsages()
-                .stream()
-                .map(usage -> usage.getUsedMetadataSchema().getUuid())
-                .toList()
-        );
-        dto.setChildren(resourceDefinition.getChildren()
-                .stream()
-                .map(child -> new ResourceDefinitionChildDTO(
-                        child.getTarget().getUuid(),
-                        child.getRelationUri(),
-                        new ResourceDefinitionChildListViewDTO(
-                                child.getTitle(),
-                                child.getTagsUri(),
-                                child.getMetadata()
-                                        .stream()
-                                        .map(
-                                                metadata -> new ResourceDefinitionChildListViewMetadataDTO(
-                                                        metadata.getTitle(), metadata.getPropertyUri()
-                                                )
-                                        ).toList()
-                        )
-                ))
-                .toList()
-        );
-        dto.setExternalLinks(List.of());  // TEMP
-// TODO:
-//        dto.setExternalLinks(resourceDefinition.getExternalLinks()
-//                .stream()
-//                .map()
-//                .toList()
-//        );
-        return dto;
-    }
-
-    @BeforeEach
-    public void setUp() {
-        // create child resource
-        ResourceDefinition child = new ResourceDefinition();
-        child.setName("child resource");
-        child.setUrlPrefix("child");
-        child.setChildren(List.of());
-        child.setParents(List.of());
-        child.setExternalLinks(List.of());
-        child.setMetadataSchemaUsages(List.of());
-        child = testEntityManager.persist(child);
-        uuids.put("child", child.getUuid());
-        // create parent resource
-        ResourceDefinition parent = new ResourceDefinition();
-        parent.setName("parent resource");
-        parent.setUrlPrefix("parent");
-        parent.setExternalLinks(List.of());
-        parent.setMetadataSchemaUsages(List.of());
-        parent = testEntityManager.persist(parent);
-        uuids.put("parent", parent.getUuid());
-        // create parent-child relation
-        ResourceDefinitionChild relation = new ResourceDefinitionChild();
-        relation.setRelationUri("http://example.org/relation");
-        relation.setTitle("relation");
-        relation.setOrderPriority(1);
-        relation.setSource(parent);
-        relation.setTarget(child);
-        relation = testEntityManager.persist(relation);
-        uuids.put("relation", relation.getUuid());
-        // manage both sides of bidirectional relations
-        parent.setChildren(List.of(relation));
-        child.setParents(List.of(relation));
-        // create relation metadata
-        ResourceDefinitionChildMetadata relationMetadata = new ResourceDefinitionChildMetadata();
-        relationMetadata.setTitle("relation metadata");
-        relationMetadata.setPropertyUri("http://example.org/property");
-        relationMetadata.setOrderPriority(1);
-        relationMetadata.setChild(relation);
-        relationMetadata = testEntityManager.persist(relationMetadata);
-        relation.setMetadata(List.of(relationMetadata));
-        // flushed by transaction
     }
 
     @Test
     @WithMockUser(roles="ADMIN")
-    public void testUpdate() throws BindException {
-        final UUID parentUuid = uuids.get("parent");
-        ResourceDefinitionChangeDTO changeDTO = createResourceDefinitionChangeDTO(parentUuid);
-        System.out.println(changeDTO.getName());
-        resourceDefinitionService.update(parentUuid, changeDTO);
-        ResourceDefinition updatedResourceDefinition = testEntityManager.find(ResourceDefinition.class, parentUuid);
-        assertEquals(1,  updatedResourceDefinition.getChildren().size());
+    public void testUpdateDoesNotDuplicateChildren() throws BindException {
+        // reproduces #830
+        final UUID uuid = KnownUUIDs.RD_CATALOG_UUID;
+        ResourceDefinition resourceDefinition = testEntityManager.find(ResourceDefinition.class, uuid);
+        ResourceDefinitionChangeDTO changeDTO = resourceDefinitionMapper.toChangeDTO(resourceDefinition);
+        resourceDefinitionService.update(uuid, changeDTO);
+        testEntityManager.refresh(resourceDefinition);
+        assertEquals(1,  resourceDefinition.getChildren().size());
     }
 }
