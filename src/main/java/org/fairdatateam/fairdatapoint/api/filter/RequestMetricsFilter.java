13f840d5e313ff32ab75cd2d114dc1516e106445
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-01-27 18:20:57 +0100

    add rudimentary RequestMetricsFilter
    Creates a custom actuator metric that counts requests to custom endpoints

---
+++ src/main/java/org/fairdatateam/fairdatapoint/api/filter/RequestMetricsFilter.java
@@ -0,0 +1,49 @@
+package org.fairdatateam.fairdatapoint.api.filter;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class RequestMetricsFilter extends OncePerRequestFilter {
+
+    private MeterRegistry meterRegistry;
+
+    public RequestMetricsFilter(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    @Override
+    public void doFilterInternal(
+            final HttpServletRequest request,
+            final HttpServletResponse response,
+            final FilterChain filterChain
+    ) throws IOException, ServletException {
+        final String uri = request.getRequestURI();
+        final String method = request.getMethod();
+        String query = request.getQueryString();
+        if (query == null) {
+            query = "";
+        }
+        String path = request.getServletPath();
+        if (path == null) {
+            path = "";
+        }
+        log.debug("Actuator counting request for {}", uri);
+        meterRegistry.counter(
+                "custom",
+                "method", method,
+                "path", path,
+                "query", query
+        ).increment();
+        filterChain.doFilter(request, response);
+    }
+}
