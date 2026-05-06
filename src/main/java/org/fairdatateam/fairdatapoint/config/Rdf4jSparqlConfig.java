7cb7dd5d451644962a011b2423fc6b4849044f0e
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-05-06 17:41:50 +0200

    rename Rdf4jConfig to Rdf4jSparqlConfig
    to disambiguate from the RepositoryConfig, which should actually be called Rdf4jRepositoryConfig

--- src/main/java/org/fairdatateam/fairdatapoint/config/Rdf4jSparqlConfig.java
+++ src/main/java/org/fairdatateam/fairdatapoint/config/Rdf4jSparqlConfig.java
@@ -3,7 +3,10 @@ package org.fairdatateam.fairdatapoint.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Make sure the SparqlQueryEvaluator implementation bean can be found (SparqlQueryEvaluatorDefault)
+ */
 @Configuration
 @ComponentScan("org.eclipse.rdf4j.http.server.readonly.sparql")
-public class Rdf4jConfig {
+public class Rdf4jSparqlConfig {
 }
