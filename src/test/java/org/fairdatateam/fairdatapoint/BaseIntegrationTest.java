bdc714467e8f5e38ef043d30cd818378f3f5b8fa
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-05-21 09:42:36 +0200

    remove allow-bean-definition-overriding from BaseIntegrationTest
    no longer necessary after removing the test config classes in #902

--- src/test/java/org/fairdatateam/fairdatapoint/BaseIntegrationTest.java
+++ src/test/java/org/fairdatateam/fairdatapoint/BaseIntegrationTest.java
@@ -29,6 +29,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles(Profiles.TESTING)
-@SpringBootTest(properties = {"spring.main.allow-bean-definition-overriding=true"})
+@SpringBootTest
 public abstract class BaseIntegrationTest {
 }
