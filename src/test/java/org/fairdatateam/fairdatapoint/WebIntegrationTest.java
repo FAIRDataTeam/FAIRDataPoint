155fa28ef5513bd4fc997aaf9fecd87bfe05e4ff
Dennis <29799340+dennisvang@users.noreply.github.com>
2026-06-10 12:06:38 +0200

    Clean up annotations for application class and test classes (#888)
    * remove @ComponentScan in favor of @SpringBootApplication and remove the redundant org.fairdatateam.fairdatapoint
    
    * remove @EnableWebMvc in favor of the existing WebConfig (WebMvcConfigurer) class
    
    * rename WebConfig to WebMvcConfig
    
    * remove @EnableAsync in favor of the existing AsyncConfig class
    
    * remove redundant package name from @ConfigurationPropertiesScan
    
    * move the rdf4j sparql component scan to a separate Rdf4jSparqlConfig class (note that RepositoryConfig should actually be called Rdf4jRepositoryConfig, for consistency)
    
    * remove allow-bean-definition-overriding from BaseIntegrationTest (no longer necessary after removing the test config classes in #902)
    
    * remove SpringExtension from test base classes because this is already included in @SpringBootTest
    
    * re-enable async processing in index trigger and ping tests
    
    * remove unused @AutoConfigureMockMvc from WebIntegrationTest class because MockMvc is not used anywhere
    
    * switch from WebEnvironment.DEFINED_PORT to WebEnvironment.RANDOM_PORT in WebIntegrationTest class to prevent issues with occupied port
    
    see spring-boot testing docs for more info: https://docs.spring.io/spring-boot/reference/testing/spring-boot-applications.html

--- src/test/java/org/fairdatateam/fairdatapoint/WebIntegrationTest.java
+++ src/test/java/org/fairdatateam/fairdatapoint/WebIntegrationTest.java
@@ -25,18 +25,14 @@ package org.fairdatateam.fairdatapoint;
 import org.fairdatateam.fairdatapoint.database.mongo.migration.development.MigrationRunner;
 import org.fairdatateam.fairdatapoint.database.rdf.migration.development.RdfDevelopmentMigrationRunner;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles(Profiles.TESTING)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@AutoConfigureMockMvc
+// todo: in most cases we can probably use WebEnvironment.MOCK with MockMvc instead of TestRestTemplate (see #862)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class WebIntegrationTest {
 
     public static final String ADMIN_TOKEN = "Bearer eyJhbGciOiJIUzUxMiJ9" +
