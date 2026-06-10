59bd2f984536b9ff8b03ca8dedd6644beba3ed2b
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-06-10 23:21:53 +0200

    add spring-boot-starter-restclient and corresponding test starter
    and fix corresponding RestTemplateBuilder import
    and replace deprecated setConnectionTimeout and setReadTimeout calls
    
    https://docs.spring.io/spring-boot/3.4/api/java/org/springframework/boot/web/client/RestTemplateBuilder.html

--- src/main/java/org/fairdatateam/fairdatapoint/config/HttpClientConfig.java
+++ src/main/java/org/fairdatateam/fairdatapoint/config/HttpClientConfig.java
@@ -22,7 +22,7 @@
  */
 package org.fairdatateam.fairdatapoint.config;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -38,8 +38,8 @@ public class HttpClientConfig {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
-                .setConnectTimeout(TIMEOUT)
-                .setReadTimeout(TIMEOUT)
+                .connectTimeout(TIMEOUT)
+                .readTimeout(TIMEOUT)
                 .build();
     }
 
