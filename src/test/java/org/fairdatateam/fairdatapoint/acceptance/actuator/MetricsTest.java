1aa68ffed5dacf60bd748682110e6282f622489f
Dennis <29799340+dennisvang@users.noreply.github.com>
2026-06-12 14:36:06 +0200

    Custom actuator metric with request counter (#836)
    * bump version to 1.20.0 (metrics are a new feature)
    
    * expose actuator metrics endpoint, but only enable http.server.requests metrics
    
      this only works after the configuration changes from #906
    
    * add ActuatorConfig to customize the http.server.requests metric (so it shows all individual resource endpoints instead of grouping them by pattern)
      
      Here we override the DefaultServerRequestObservationConvention instead of subclassing the ObservationFilter
      Based on advise by micrometer devs.
    
    * add TestMetrics class with metrics filter check
    * test values of http.url tag
    
    * test that endpoint visits are counted
    
    * require authorization for actuator metrics endpoints
    
    * test unauthenticated access to actuator metrics
    
    * accept header not necessary for metrics auth test
    
    * test auth for metric uri instead of metric list uri, so we can be sure the authentication requirement also applies to sub-paths of /actuator/metrics

---
+++ src/test/java/org/fairdatateam/fairdatapoint/acceptance/actuator/MetricsTest.java
@@ -0,0 +1,148 @@
+/**
+ * The MIT License
+ * Copyright © 2017 FAIR Data Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.fairdatateam.fairdatapoint.acceptance.actuator;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.fairdatateam.fairdatapoint.Profiles;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.assertj.MvcTestResult;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@ActiveProfiles(Profiles.TESTING)
+@AutoConfigureMockMvc
+@SpringBootTest
+@WithMockUser
+class MetricsTest {
+
+    final String metricName = "http.server.requests";
+    final UriComponentsBuilder metricsUriBuilder = UriComponentsBuilder.fromPath("/actuator/metrics");
+    final String tagName = "http.url";
+    final String uriVisited = "/meta";
+
+    private final MeterRegistry meterRegistry;
+
+    // https://docs.spring.io/spring-framework/reference/testing/mockmvc/assertj.html
+    private final MockMvcTester mockMvc;
+
+    /**
+     * Constructor
+     */
+    @Autowired
+    MetricsTest(MeterRegistry meterRegistry, MockMvcTester mockMvc) {
+        this.meterRegistry = meterRegistry;
+        this.mockMvc = mockMvc;
+    }
+
+    @BeforeEach
+    void clearMeterRegistry() {
+        // clear all metrics
+        meterRegistry.clear();
+    }
+
+    @Test
+    @WithAnonymousUser
+    void onlyAuthenticatedUsersCanAccessMetrics() {
+        MvcTestResult testResult = mockMvc.get()
+                .uri(metricsUriBuilder.pathSegment(metricName).build().toUri())
+                .exchange();
+        // todo: should actually be 401 UNAUTHORIZED (see #704)
+        assertThat(testResult).hasStatus(HttpStatus.FORBIDDEN);
+    }
+
+    @Test
+    void onlyExposesHttpServerRequestsMetric() {
+        // visit some endpoint to initialize the "http.server.requests" metric
+        mockMvc.get().uri(uriVisited).exchange();
+        // get metrics list
+        MvcTestResult testResult = mockMvc.get()
+                .uri(metricsUriBuilder.build().toUri())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange();
+        // results should only include the "http.server.requests" (this only appears if endpoints have been visited)
+        assertThat(testResult)
+                .hasStatusOk()
+                .hasContentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+                .bodyJson()
+                .extractingPath("$.names")
+                .asArray()
+                .containsExactly(metricName);
+    }
+
+    @Test
+    void listsVisitedEndpointUris() {
+        // visit some endpoint to initialize the "http.server.requests" metric
+        mockMvc.get().uri(uriVisited).exchange();
+        // get the available metrics for "http.server.requests"
+        MvcTestResult testResult = mockMvc.get()
+                .uri(metricsUriBuilder.pathSegment(metricName).build().toUri())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange();
+        // the visited endpoint should be included in the available tag values for "http.url"
+        assertThat(testResult).hasStatusOk().hasContentTypeCompatibleWith(MediaType.APPLICATION_JSON);
+        assertThat(testResult).bodyJson().extractingPath("$.name").isEqualTo(metricName);
+        assertThat(testResult).bodyJson().extractingPath("$.availableTags").asArray()
+                .filteredOn("tag", tagName)
+                .flatExtracting("values")
+                .contains(uriVisited);
+    }
+
+    @Test
+    void endpointVisitsAreCounted() {
+        // visit some endpoint a number of times
+        final int visitCount = 3;
+        for (int i = 0; i < visitCount; i++) {
+            mockMvc.get().uri(uriVisited).exchange();
+        }
+        // get the available metrics for this endpoint
+        URI endpointMetricsUri = metricsUriBuilder
+                .pathSegment(metricName)
+                .queryParam("tag", "%s:%s".formatted(tagName, uriVisited))
+                .build().toUri();
+        MvcTestResult testResult = mockMvc.get()
+                .uri(endpointMetricsUri)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange();
+        // the COUNT statistic value for this endpoint should match the actual number of visits
+        assertThat(testResult).hasStatusOk().hasContentTypeCompatibleWith(MediaType.APPLICATION_JSON);
+        assertThat(testResult).bodyJson().extractingPath("$.name").isEqualTo(metricName);
+        assertThat(testResult).bodyJson().extractingPath("$.measurements").asArray()
+                .filteredOn("statistic", "COUNT")
+                .extracting("value")
+                .contains((double) visitCount);
+    }
+}
