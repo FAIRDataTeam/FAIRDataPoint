c3224f919d00a7e2857ef47ad33e5affb32a2fde
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-06-16 16:19:29 +0200

    add CorsTest class

---
+++ src/test/java/org/fairdatateam/fairdatapoint/acceptance/general/CorsTest.java
@@ -0,0 +1,84 @@
+/**
+ * The MIT License
+ * Copyright © 2017 FAIR Data Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.fairdatateam.fairdatapoint.acceptance.general;
+
+import org.fairdatateam.fairdatapoint.Profiles;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.assertj.MvcTestResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Verifies Cross-Origin Resource Sharing (CORS) configuration. For more information see
+ * <a href="https://docs.spring.io/spring-security/reference/servlet/integrations/cors.html">spring security CORS</a>
+ * and <a href="https://docs.spring.io/spring-framework/reference/web/webmvc-cors.html">spring web mvc CORS</a>.
+ */
+@ActiveProfiles(Profiles.TESTING)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class CorsTest {
+
+    private final String otherOrigin = "https://other.origin";
+    private final String uri = "/";
+
+    private final MockMvcTester mockMvc;
+
+    /**
+     * Constructor
+     */
+    @Autowired
+    public CorsTest(MockMvcTester mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    public void corsPreflightRequestFailsIfAccessControlRequestMethodHeaderMissing() {
+        MvcTestResult testResult = mockMvc.options()
+                .uri(uri)
+                .header(HttpHeaders.ORIGIN, otherOrigin)
+                .exchange();
+        assertThat(testResult).hasStatus(HttpStatus.FORBIDDEN)
+                .hasBodyTextEqualTo("Invalid CORS request");
+    }
+
+    @Test
+    public void corsPreflightRequestYieldsExpectedAccessControlHeaders() {
+        MvcTestResult testResult = mockMvc.options()
+                .uri(uri)
+                // this preflight says: "I plan to make a GET request."
+                .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, HttpMethod.GET)
+                .header(HttpHeaders.ORIGIN, otherOrigin)
+                .exchange();
+        assertThat(testResult).hasStatusOk()
+                .hasHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+    }
+
+}
