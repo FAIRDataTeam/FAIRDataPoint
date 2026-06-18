12f1d2551022980691338197d89dce3e38f3a947
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-06-18 11:43:36 +0200

    fix imports for AutoConfigureMockMvc and AutoConfigureMockRestServiceServer
    these have been moved from org.springframework.boot.test.autoconfigure.web to two different packages

--- src/test/java/org/fairdatateam/fairdatapoint/acceptance/general/CorsTest.java
+++ src/test/java/org/fairdatateam/fairdatapoint/acceptance/general/CorsTest.java
@@ -26,7 +26,7 @@ import org.eclipse.jetty.http.HttpHeader;
 import org.fairdatateam.fairdatapoint.Profiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
