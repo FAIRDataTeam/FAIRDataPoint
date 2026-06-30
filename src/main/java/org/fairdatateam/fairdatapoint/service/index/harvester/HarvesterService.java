516d26dcb3e4d0e2615b9bedbfd4b21af591f4c7
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-06-30 18:28:06 +0200

    replace RestTemplate by RestClient in HarvesterService
    and switch to constructor autowiring
    
    see https://docs.spring.io/spring-framework/reference/integration/rest-clients.html#migrating-to-restclient

--- src/main/java/org/fairdatateam/fairdatapoint/service/index/harvester/HarvesterService.java
+++ src/main/java/org/fairdatateam/fairdatapoint/service/index/harvester/HarvesterService.java
@@ -32,13 +32,12 @@ import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.vocabulary.LDP;
 import org.eclipse.rdf4j.model.vocabulary.RDF;
 import org.eclipse.rdf4j.rio.RDFFormat;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
 
@@ -57,11 +56,17 @@ public class HarvesterService {
 
     private static final String DEFAULT_NAVIGATION_SHACL = "defaultNavigationShacl.ttl";
 
-    @Autowired
-    private GenericMetadataRepository genericMetadataRepository;
+    private final GenericMetadataRepository genericMetadataRepository;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestClient client;
+
+    /**
+     * Constructor (autowired)
+     */
+    public HarvesterService(GenericMetadataRepository genericMetadataRepository, RestClient client) {
+        this.genericMetadataRepository = genericMetadataRepository;
+        this.client = client;
+    }
 
     public void deleteHarvestedData(String clientUrl) throws MetadataRepositoryException {
         genericMetadataRepository.remove(i(clientUrl));
@@ -137,12 +142,14 @@ public class HarvesterService {
 
     private Model makeRequest(String uri) {
         log.info("Making request to '{}'", uri);
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(List.of(MediaType.parseMediaType(RDFFormat.TURTLE.getDefaultMIMEType())));
-        final HttpEntity<Void> entity = new HttpEntity<>(null, headers);
         try {
             final ResponseEntity<String> response =
-                    restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+                    client.method(HttpMethod.GET)
+                            .uri(uri)
+                            .headers(httpHeaders -> httpHeaders.setAccept(
+                                    List.of(MediaType.parseMediaType(RDFFormat.TURTLE.getDefaultMIMEType()))))
+                            .retrieve()
+                            .toEntity(String.class);
             if (!response.getStatusCode().is2xxSuccessful()) {
                 log.info("Request to '{}' failed ({})", uri, response.getStatusCode());
                 throw new HttpClientErrorException(response.getStatusCode());
