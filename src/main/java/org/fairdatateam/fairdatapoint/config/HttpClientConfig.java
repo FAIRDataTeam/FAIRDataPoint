4f8118bae1256db08a81a44880a8b535370544a0
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-08-14 11:32:07 +0200

    replace HttpClientSettings by global spring.http.clients config
    https://docs.spring.io/spring-boot/reference/io/rest-client.html#io.rest-client.global-configuration

--- src/main/java/org/fairdatateam/fairdatapoint/config/HttpClientConfig.java
+++ src/main/java/org/fairdatateam/fairdatapoint/config/HttpClientConfig.java
@@ -22,41 +22,19 @@
  */
 package org.fairdatateam.fairdatapoint.config;
 
-import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
-import org.springframework.boot.http.client.HttpClientSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
-
-import java.time.Duration;
 
 @Configuration
 public class HttpClientConfig {
 
-    private static final Duration TIMEOUT = Duration.ofSeconds(5);
-
     @Bean
-    public RestClient restClient(RestClient.Builder builder, ClientHttpRequestFactory clientHttpRequestFactory) {
+    public RestClient restClient(RestClient.Builder builder) {
         // The builder argument is required for autoconfiguration of MockRestServiceServer in tests
         // https://docs.spring.io/spring-framework/reference/integration/rest-clients.html
-        // Even though spring autoconfig already picks up the custom HttpClientSettings bean by default,
-        // we use a custom request factory to apply the settings explicitly.
         // https://docs.spring.io/spring-boot/reference/io/rest-client.html#io.rest-client.restclient.customization
-        return builder.requestFactory(clientHttpRequestFactory).build();
-    }
-
-    @Bean
-    public HttpClientSettings httpClientSettings() {
-        return HttpClientSettings.defaults()
-                .withConnectTimeout(TIMEOUT)
-                .withReadTimeout(TIMEOUT);
-    }
-
-    @Bean
-    public ClientHttpRequestFactory clientHttpRequestFactory(HttpClientSettings httpClientSettings) {
-        // Apply the http client settings explicitly (even though spring autoconfig already takes care of this)
-        return ClientHttpRequestFactoryBuilder.detect().build(httpClientSettings);
+        return builder.build();
     }
 
 }
