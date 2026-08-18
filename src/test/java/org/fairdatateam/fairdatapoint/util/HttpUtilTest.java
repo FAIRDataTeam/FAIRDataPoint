17bbc2d787ed2f5a95f456e994dc73ee69b989ae
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-08-18 17:29:29 +0200

    move HttpUtilTest into new util test package
    consistent with main file structure

--- src/test/java/org/fairdatateam/fairdatapoint/util/HttpUtilTest.java
+++ src/test/java/org/fairdatateam/fairdatapoint/util/HttpUtilTest.java
@@ -20,7 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.fairdatateam.fairdatapoint.testutils;
+package org.fairdatateam.fairdatapoint.util;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.eclipse.rdf4j.model.IRI;
